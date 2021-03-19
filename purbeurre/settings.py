14b373adcc74e88209747b80877d126126019782
bbbenbat <bbessayah@gmail.com>
2021-03-19 22:47:02 +0100

    update allowed host to settings.py

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = get_env_variable('SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('ALD_HOST')]
+ALLOWED_HOSTS = ['188.166.11.194']
 
 # Application definition
 
