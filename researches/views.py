19942da8b33114f2ee84ee4b04cd44263249e848
bbbenbat <bbessayah@gmail.com>
2021-04-08 17:54:47 +0200

    Update views.py

--- researches/views.py
+++ researches/views.py
@@ -90,14 +90,12 @@ def profil_user(request):
 
 
 def handler404(request, *args, **argv):
-    response = render('404.html', {},
-                      context_instance=RequestContext(request))
+    response = render(request, '404.html')
     response.status_code = 404
     return response
 
 
 def handler500(request, *args, **argv):
-    response = render('500.html', {},
-                      context_instance=RequestContext(request))
+    response = render(request, '500.html')
     response.status_code = 500
     return response
